3bf0fef81f665a28afa814c6abd8f7265bb6cc48
pchmn <pchmn.dev@gmail.com>
2017-05-05 16:29:45 +0200

    Initial commit

---
+++ app/src/main/java/com/pchmn/starterpack/ui/OnFragmentActionListener.java
@@ -0,0 +1,9 @@
+package com.pchmn.starterpack.ui;
+
+import android.os.Bundle;
+
+public interface OnFragmentActionListener {
+
+    void onNextFragment(int number, Bundle bundle, boolean addToBackStack);
+
+}
---
+++ app/src/main/java/com/pchmn/starterpack/ui/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pchmn.starterpack.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pchmn.starterpack.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
